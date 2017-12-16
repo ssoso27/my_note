d8973004722c2abe8dd79381fca8b35245dfe188
ssoso27 <sohi8290@jejunu.ac.kr>
2017-12-16 17:00:35 +0900

    새노트 기능

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
 console.log('my-note app.js');
+
+function getDom(name)
+{
+  return document.getElementsByClassName(name);
+}
+
+var btn_new = getDom('btn-newnote')[0];
+var btn_save = getDom('btn-savenote')[0];
+var btn_about = getDom('btn-about')[0];
+var ta_memo = document.getElementById('memo');
+
+btn_new.addEventListener('click', function() {
+  console.log('click new note');
+  ta_memo.value = "";
+});
