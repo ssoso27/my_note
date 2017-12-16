724cd7e74c62ceec92856ea09ce872c16d12f212
ssoso27 <sohi8290@jejunu.ac.kr>
2017-12-16 17:10:02 +0900

    노트 저장

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,14 @@ btn_new.addEventListener('click', function() {
   console.log('click new note');
   ta_memo.value = "";
 });
+
+btn_save.addEventListener('click', function()
+{
+  console.log('click save button');
+  localStorage.setItem('strMemo', ta_memo.value);
+});
+
+window.addEventListener('load', function()
+{
+  ta_memo.value = localStorage.getItem('strMemo');
+});
