fd6c02e70720af5b74cb17a0a4e6ca815063c7b6
ssoso27 <sohi8290@jejunu.ac.kr>
2017-12-16 18:06:52 +0900

    요소 저장

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('my-note app.js');
+$(function()
+{
+  console.log('my-note app.js');
+
+  var btn_new = $('.btn-newnote')[0];
+  var btn_save = $('.btn-savenote')[0];
+  var btn_about = $('.btn-about')[0];
+  var ta_memo = $('#memo')[0];
+
+
+
+  console.log(btn_new);
+  
+});
