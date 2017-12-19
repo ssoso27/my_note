ed27f698b819bca8a00164b73989fc0170011748
ssoso27 <sohi8290@jejunu.ac.kr>
2017-12-20 08:30:10 +0900

    create FileSaver.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var cleanCSS = require('gulp-clean-css');
 var connect = require('gulp-connect');
 
 //js 프로젝트 소스파일
-var jsfiles = ['src/js/app.js'];
+var jsfiles = ['src/js/app.js', 'src/js/FileSaver.js', 'src/js/utils.js'];
 
 gulp.task('concat:js', function() {
   return gulp.src(jsfiles)
